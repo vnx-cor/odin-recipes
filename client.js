67222b2d64704a0ea8a6e09da016bceaed4731b5
vnx-cor <axevnx67@gmail.com>
2026-01-29 10:17:47 +0800

    Add CSS font weight to sidebar container anchor
    Added a bold font weight when the anchor is hovered and
    change to its default state when unhovered.

--- client.js
+++ client.js
@@ -6,16 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
 	container.forEach(element => {
 		element.addEventListener("mouseenter", () => {
 			// Change CSS style when hovered
-			element.style.backgroundColor = "rgba(61, 31, 16, 0.8)";
+			element.style.backgroundColor = "rgba(122, 79, 42, 0.5)";
 			element.children[0].style.color = "rgb(61, 38, 14)"; // Icon
-			element.children[1].style.color = "rgb(61, 38, 14)"; // Anchor
+
+			// Anchor
+			element.children[1].style.color = "rgb(61, 38, 14)";
+			element.children[1].style.fontWeight = "700";
 		});
 
 		element.addEventListener("mouseleave", () => {
 			// Change CSS style to default when unhovered
 			element.style.backgroundColor = "";
 			element.children[0].style.color = "black"; // Icon
-			element.children[1].style.color = "black"; // Ancho
+
+			// Anchor
+			element.children[1].style.color = "black";
+			element.children[1].style.fontWeight = "400";
 		});
 	});
 });
