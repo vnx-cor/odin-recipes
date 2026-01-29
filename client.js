a3f3a3dace6f1f59b3caa19e78f40f2e4ca49090
vnx-cor <axevnx67@gmail.com>
2026-01-29 10:11:10 +0800

    Add CSS styles to sidebar container children
    Added CSS styles to sidebar container's children when it's hovered
    or unhovered.

--- client.js
+++ client.js
@@ -7,11 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
 		element.addEventListener("mouseenter", () => {
 			// Change CSS style when hovered
 			element.style.backgroundColor = "rgba(61, 31, 16, 0.8)";
+			element.children[0].style.color = "rgb(61, 38, 14)"; // Icon
+			element.children[1].style.color = "rgb(61, 38, 14)"; // Anchor
 		});
 
 		element.addEventListener("mouseleave", () => {
 			// Change CSS style to default when unhovered
 			element.style.backgroundColor = "";
+			element.children[0].style.color = "black"; // Icon
+			element.children[1].style.color = "black"; // Ancho
 		});
 	});
 });
