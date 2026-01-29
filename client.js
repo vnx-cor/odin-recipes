937f397ee1a90974a50968e81f08f523d979edb6
vnx-cor <axevnx67@gmail.com>
2026-01-29 09:55:56 +0800

    Modify client.js file
     - Change color format of backgroundColor property
     - Add event listener to sidebar container when unhovered

--- client.js
+++ client.js
@@ -8,7 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
 	container.forEach(element => {
 		element.addEventListener("mouseenter", () => {
 			// Change CSS style when hovered
-			element.style.backgroundColor = "#5a4e43";
+			element.style.backgroundColor = "rgba(61, 31, 16, 0.8)";
+		});
+
+		element.addEventListener("mouseleave", () => {
+			// Change CSS style to default when unhovered
+			element.style.backgroundColor = "";
 		});
 	});
 });
