06a3764ee9817644402a3539ba96f65538d9a3de
vnx-cor <axevnx67@gmail.com>
2026-01-29 08:34:30 +0800

    Add target elements inside variable

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+	// Target elements for hover
+	const container = document.querySelectorAll(".sidebar__container");
+	const icon = document.querySelectorAll(".sidebar__icon");
+	const anchor = document.querySelectorAll(".sidebar__anchor");
 });
