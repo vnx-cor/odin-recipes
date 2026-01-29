2c5d3bfe6b4beee0f9a2ffb9916715b7904a7160
vnx-cor <axevnx67@gmail.com>
2026-01-29 08:38:15 +0800

    Add event listener to sidebar container

--- client.js
+++ client.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	const container = document.querySelectorAll(".sidebar__container");
 	const icon = document.querySelectorAll(".sidebar__icon");
 	const anchor = document.querySelectorAll(".sidebar__anchor");
+
+	// Trigger function when sidebar container is hovered
+	container.addEventListener("mouseenter", function() {
+
+	});
 });
